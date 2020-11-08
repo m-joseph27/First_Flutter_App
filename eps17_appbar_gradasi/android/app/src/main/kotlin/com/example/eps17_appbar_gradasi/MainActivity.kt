72a578fb24e0ebe129a0ede369489127aaedf680
m-joseph27 <muh.joseph27@gmail.com>
2020-11-08 17:59:19 +0700

    Gradation AppBar

---
+++ eps17_appbar_gradasi/android/app/src/main/kotlin/com/example/eps17_appbar_gradasi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eps17_appbar_gradasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ eps17_appbar_gradasi/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.adb, color: Colors.white,),
+          title: Text('latihan Appbar gradasi', style: TextStyle(color: Colors.white),
+          ),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.settings), onPressed: () {},),
+          IconButton(icon: Icon(Icons.exit_to_app), onPressed: () {},),
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: FractionalOffset.topLeft,
+              end: FractionalOffset.bottomRight
+            ),
+            image: DecorationImage(
+              image: AssetImage('assets/images.png'),
+              fit: BoxFit.none,
+              repeat: ImageRepeat.repeat
+            ),
+          ),
+        ),
+        ),
+        
+      ),
+    );
+  }
+}
