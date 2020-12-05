d7ce5b230ad8e432afa465d8e0bbcb0d03cd9c07
m-joseph27 <muh.joseph27@gmail.com>
2020-12-05 23:22:37 +0700

    create tapbar

---
+++ eps_27_tapbar/android/app/src/main/kotlin/com/example/eps_27_tapbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eps_27_tapbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ eps_27_tapbar/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+              title: Text('Tap Bar'),
+              bottom: TabBar(
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.comment),
+                    text: 'Comments',
+                  ),
+                  Tab(
+                    child: Image(
+                      image: AssetImage('assets/ironman.png'),
+                    ),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.computer),
+                  ),
+                  Tab(
+                    text: 'News',
+                  )
+                ],
+              )),
+          body: TabBarView(
+            children: [
+              Center(
+                child: Text('Tab 1'),
+              ),
+              Center(
+                child: Text('Tab 2'),
+              ),
+              Center(
+                child: Text('Tab 3'),
+              ),
+              Center(
+                child: Text('Tab 4'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
